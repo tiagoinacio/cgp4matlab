7497e7de2d20ecf97bf339296f17ad29bd5ed3d4
Tiago Inácio <tiago.inacio@blip.pt>
2016-10-10 15:31:11 +0100

    update file structure

--- test.m
+++
@@ -1,6 +0,0 @@
-listOfSpecs = dir([pwd, '/tests/']);
-listOfSpecs = listOfSpecs(~ismember({listOfSpecs.name},{'.','..'}));
-
-for i = 1:length(listOfSpecs)
-   runtests([listOfSpecs(i).name, 'Spec'])
-end
